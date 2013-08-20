f38c2128677c5c6181fe3b40d68de1c9bc371b4e
dan peade <dan.peade@gmail.com>
2013-08-20 16:29:41 +1000

    Added some whitespace to test post recieve hook

--- toggl2youtrack.py
+++ toggl2youtrack.py
@@ -36,6 +36,7 @@ def login_to_youtrack(username, password):
     return result.headers['set-cookie']
 
 
+
 def get_youtrack_time_entries(cookie_header, task_id):
     result = requests.get(
         '{base_api_url}issue/{task_id}/timetracking/workitem/'.format(base_api_url=config['youtrack']['base_api_url'], task_id=task_id),
